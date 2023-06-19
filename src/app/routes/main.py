7c0e0744b996b0d4295c1d354dee96f489165365
AOI_1up <aoi_1up@icloud.com>
2023-06-19 14:26:26 +0000

    [Feat] 問題作成ページの追加

--- src/app/routes/main.py
+++ src/app/routes/main.py
@@ -2,7 +2,14 @@ from flask import Blueprint, render_template
 
 main_bp = Blueprint("main", __name__)
 
-# ホームページ API
+
+# Top ページ用 API
 @main_bp.route("/", methods=["GET"])
 def index_get():
     return render_template("index.html")
+
+
+# 問題作成用 API
+@main_bp.route("/question", methods=["GET"])
+def create_question_get():
+    return render_template("question.html")
