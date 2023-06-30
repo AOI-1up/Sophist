382ddc7efa8834513085f4f0616a6b74e90cba96
AOI_1up <aoi_1up@icloud.com>
2023-06-30 10:27:11 +0000

    [Feat] 問題作成ページの追加

--- src/app/models.py
+++ src/app/models.py
@@ -3,14 +3,14 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
 
 
-# ユーザモデルを定義
+# ユーザのモデルを定義
 class User(UserMixin, db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)    # ユーザ ID (主キー)
     name = db.Column(db.String(128))                # 名前
     mail = db.Column(db.String(128), unique=True)   # メールアドレス
     password = db.Column(db.String(128))            # パスワード
-    creator = db.relationship(
+    creator = db.relationship(                      # 問題との関係
         'QuestionList',
         backref='user',
         lazy=True
@@ -25,23 +25,24 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.password, password)
 
 
-# 問題リストモデルを定義
+# 問題リストのモデルを定義
 class QuestionList(db.Model):
     __tablename__ = 'question_list'
     id = db.Column(db.Integer, primary_key=True)    # 問題リスト ID (主キー)
-    creator_id = db.Column(
+    creator_id = db.Column(                         # 作成者 ID (外部キー)
         db.Integer,
         db.ForeignKey('user.id'),
         nullable=False
     )
-    questions = db.relationship(
+    list_title = db.Column(db.String(128))          # 問題リストのタイトル
+    questions = db.relationship(                    # 問題との関係 (1:多)
         'Question',
         backref='question_list',
         lazy=True
     )
 
 
-# 問題モデルを定義
+# 問題のモデルを定義
 class Question(db.Model):
     __tablename__ = 'question'
     id = db.Column(db.Integer, primary_key=True)    # 問題 ID (主キー)
@@ -69,72 +70,3 @@ class Option(db.Model):
         nullable=False
     )
     option_text = db.Column(db.String(128))         # 選択肢の内容
-
-
-"""
-# 問題リストモデルを定義
-class QuestionList(db.Model):
-    id = db.Column(db.Integer, primary_key=True)    # 問題リストID（主キー）
-    list_title = db.Column(db.String(128))          # 問題リストのタイトル
-    creator_id = db.Column(                         # 作成者のユーザID（外部キー）
-        db.Integer,
-        db.ForeignKey(User.id)
-    )
-    creator = db.relationship(                          # 作成者とのリレーション
-        User,
-        backref=db.backref('question_lists', lazy=True)
-    )
-
-
-
-# 問題モデルを定義
-class Question(db.Model):
-    id = db.Column(db.Integer, primary_key=True)   # 問題ID（主キー）
-    question_content = db.Column(db.String(128))            # 問題のタイトル
-    choices = db.relationship(                              # 選択肢とのリレーション
-        'Choice',
-        backref=db.backref('question', lazy=True)
-    )
-    correct_answer = db.relationship(                       # 正解とのリレーション
-        'CorrectAnswer',
-        backref=db.backref('question', uselist=False),
-    )
-    question_list_id = db.Column(                           # 問題リストID（外部キー）
-        db.Integer,
-        db.ForeignKey('question_list.id')
-    )
-
-
-# 解答結果モデルを定義
-class AnswerResult(db.Model):
-    id = db.Column(db.Integer, primary_key=True)     # 解答結果ID（主キー）
-    user_id = db.Column(db.Integer, db.ForeignKey(
-        'user.id'))                                         # ユーザID（外部キー）
-    question_id = db.Column(db.Integer, db.ForeignKey(
-        'question.question_id'))                            # 問題ID（外部キー）
-    selected_choice = db.Column(db.String(128))             # 選んだ選択肢
-
-
-# 選択肢モデルを定義
-class Choice(db.Model):
-    id = db.Column(db.Integer, primary_key=True)     # 選択肢ID（主キー）
-    question_id = db.Column(db.Integer, db.ForeignKey(      # 問題ID（外部キー）
-        'question.question_id'))
-    choice_text = db.Column(db.String(128))                 # 選択肢のテキスト
-
-
-# 正解モデルを定義
-class CorrectAnswer(db.Model):
-    id = db.Column(db.Integer, primary_key=True)             # 正解ID（主キー）
-    question_id = db.Column(db.Integer, db.ForeignKey(              # 問題ID（外部キー）
-        'question.question_id'))
-    choice_id = db.Column(db.Integer, db.ForeignKey(                # 選択肢ID（外部キー）
-        'choice.choice_id'))
-    question_rel = db.relationship(
-        'Question',
-        backref=db.backref('correct_answer_rel', uselist=False),
-        overlaps="correct_answer"
-    )
-    # 選択肢とのリレーション
-    choice = db.relationship('Choice')
-"""
--- src/app/routes/main.py
+++ src/app/routes/main.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, jsonify, redirect, url_for
 from flask_login import current_user
-
+from app import db
+from app.models import QuestionList, Question, Option
 
 main_bp = Blueprint("main", __name__)
 
@@ -23,7 +24,43 @@ def create_question_get():
 def create_question_post():
     form_data = request.form.to_dict()
 
-    for key, value in form_data.items():
-        if key.startswith("question"):
-            print(form_data)
-    return jsonify(form_data)
+    # 問題リストの内容を格納
+    creator_id = current_user.id
+    list_title = form_data.get("list_title")
+    question_list = QuestionList(creator_id=creator_id, list_title=list_title)
+    db.session.add(question_list)
+    db.session.commit()
+
+    # 問題の内容を格納
+    list_id = question_list.id
+    question_keys = [key for key in form_data.keys() if key.startswith("question")]
+    answer_list = [key for key in form_data.keys() if key.startswith("answer")]
+    for key in question_keys:
+        question_text = form_data.get(key)
+        correct_answer = form_data.get(answer_list[question_keys.index(key)])
+        question = Question(
+            list_id=list_id, question_text=question_text, correct_answer=correct_answer
+        )
+        db.session.add(question)
+    db.session.commit()
+
+    # 選択肢の内容を格納
+    question_id = question.id
+    option_keys = [key for key in form_data.keys() if key.startswith("option")]
+    for key in option_keys:
+        option_text = form_data.get(key)
+        option = Option(question_id=question_id, option_text=option_text)
+        db.session.add(option)
+    db.session.commit()
+
+    return redirect(url_for("main.question_content_get", list_id=list_id))
+
+
+# 問題回答用 API
+@main_bp.route("/question/<list_id>", methods=["GET"])
+def question_content_get(list_id):
+    list = QuestionList.query.get(list_id)
+    list_title = list.list_title
+    return render_template(
+        "question_content.html", list_title=list_title
+    )
